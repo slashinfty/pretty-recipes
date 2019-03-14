12bea11cd0f7980cfd60d03e1e80d9f705f9a2b6
Matt Braddock <matt.braddock@gmail.com>
2019-03-13 22:08:26 -0400

    literal strings

--- latex.js
+++ latex.js
@@ -1,11 +1,11 @@
-var one = "\documentclass[12pt]{letter}\usepackage[letterpaper,margin=0.65in]{geometry}\usepackage{textcomp}\pagenumbering{gobble}\begin{document}\begin{huge}";//then title
-var two = "\end{huge}\newline\vspace{-2.5mm}\newline\renewcommand{\arraystretch}{1.1}\begin{tabular*}{\textwidth}{@{\extracolsep{\fill}}lr}";//then description
-var three = "&";//then prep time
-var four = "\\";//then author
-var five = "&";//then cook time
-var six = "\end{tabular*}\newline\vspace{10mm}\newline\begin{huge}Ingredients\end{huge}\\\rule[2.8mm]{\textwidth}{.1pt}\vspace{-3mm}\begin{itemize}";//then start ingredient list
-var seven = "\end{itemize}\vspace{7mm}\begin{huge}Directions\end{huge}\\\rule[2.8mm]{\textwidth}{.1pt}\vspace{-3mm}\begin{enumerate}";//then start directions list
-var eight = "\end{enumerate}\end{document}";
+var one = `\documentclass[12pt]{letter}\usepackage[letterpaper,margin=0.65in]{geometry}\usepackage{textcomp}\pagenumbering{gobble}\begin{document}\begin{huge}`;//then title
+var two = `\end{huge}\newline\vspace{-2.5mm}\newline\renewcommand{\arraystretch}{1.1}\begin{tabular*}{\textwidth}{@{\extracolsep{\fill}}lr}`;//then description
+var three = `&`;//then prep time
+var four = `\\`;//then author
+var five = `&`;//then cook time
+var six = `\end{tabular*}\newline\vspace{10mm}\newline\begin{huge}Ingredients\end{huge}\\\rule[2.8mm]{\textwidth}{.1pt}\vspace{-3mm}\begin{itemize}`;//then start ingredient list
+var seven = `\end{itemize}\vspace{7mm}\begin{huge}Directions\end{huge}\\\rule[2.8mm]{\textwidth}{.1pt}\vspace{-3mm}\begin{enumerate}`;//then start directions list
+var eight = `\end{enumerate}\end{document}`;
 
 function latexReplace (str) {
     if (str === "") return;
