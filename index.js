cf7499a017fb47b0b500e616503f3528e4095971
Matt Braddock <matt.braddock@gmail.com>
2019-03-19 13:54:49 -0400

    https fetch

--- index.js
+++ index.js
@@ -42,6 +42,6 @@ async function submit() {
     });
     let document = one + latexReplace(title) + two + latexReplace(descript) + three + prep + four + latexReplace(author) + five + cook + six + ingredientString + seven + directionString + eight;
     let submission = encodeURIComponent(document.trim());
-    let response = await fetch("http://latex-online-recipe-latex.7e14.starter-us-west-2.openshiftapps.com/compile?text=" + submission);
+    let response = await fetch("https://latex-online-recipe-latex.7e14.starter-us-west-2.openshiftapps.com/compile?text=" + submission);
     await saveAs(new Blob([response], {type: 'application/pdf'}), title.trim().replace(/\s/g, "-") + ".pdf");
 }
