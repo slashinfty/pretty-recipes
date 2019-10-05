8cce69c03421745da8e8fed2aa685ae7bae8f89a
slashinfty <matt.braddock@gmail.com>
2019-10-05 18:44:33 -0400

    regex replace filename

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ var prepTime;
 var cookTime;
 
 function submit() {
-    let filename = title.trim().replace(/\s/g, "-").toLowerCase();
+    let filename = title.trim().replace(/\s/g, "-").replace(/\/\\:\*\?\"<\>\|/, "").toLowerCase();
     let prep = prepTime !== ("" || undefined) ? "Prep: " + latexReplace(prepTime) : "";
     let cook = cookTime !== ("" || undefined) ? "Cook: " + latexReplace(cookTime) : "";
     let ingredientString = "";
